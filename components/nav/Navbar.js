17f7a87f18202c34a09635c8f05f362bab2d2e05
Flower Dev <67862441+Flower-dev@users.noreply.github.com>
2022-07-13 00:15:47 +0200

    fix bug

--- components/nav/Navbar.js
+++ components/nav/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
             <Link href={'/'}>
                 <a>Home</a>
             </Link>
-            <Link href={'/'}>
+            <Link href={'/about'}>
                 <a>About</a>
             </Link>
             <Link href={'/contact'}>
--- pages/contact.js
+++ pages/contact.js
@@ -3,12 +3,18 @@ import HeaderPage from '../components/layouts/Header';
 // styles
 import styles from '../styles/Home.module.css'
 
-export default function Conctact() {
+export default function Contact() {
     return (
         <div className={styles.container}>
             <HeaderPage titleHead='Institut du Design | Contact'/>
             <main>
-                <p>test contact</p>
+                <h3>Réalisons ensemble un magnifique projet</h3>
+                <h4>Envoyez nous un message</h4>
+                <p>
+                    pour une réponse adaptée, merci de préciser tous les détails 
+                    qui nous permettront de mieux appréhender vos besoins 
+                    (surface totale du bien, surface à étudier, description du projet, budget) 
+                </p>
             </main>
         </div>
     )
